6131d5691d004e01a775f1947c2fcdc87b708387
ArtificialBlue <oceantraveler201@gmail.com>
2020-10-05 01:45:19 -0700

    displayed cart items on console

--- mood-shop/resources/js/scripts.js
+++ mood-shop/resources/js/scripts.js
@@ -40,19 +40,52 @@ for (let i=0; i<data.length; ++i) {
 const cart = []
 
 function addItem(name,price){
-    const item = {name:name, price: price, qty: 1 }
+
+    for (let i = 0; i < cart.length; i += 1){
+        if (cart[i].name === name){
+            cart[i].qty += 1
+            return
+        }
+    }
+
+
+
+    const item = {name, price, qty: 1 }
     cart.push(item)
 }
 
 function showItems(){
 //Template Strings have to have grave accent characters: `` and not single quotes ''
-    console.log(`You have ${cart.length} items in your cart`)
+    const qty = getQty()
+    console.log(`You have ${qty} items in your cart`)
+    for(let i = 0; i < cart.length; i += 1){
+        console.log(`- ${cart[i].name } $${cart[i].price} x ${cart[i].qty} `)
+
+    }
+    const total = getTotal()
+    console.log(`Total in cart: $${total.toFixed(2)}`)
+
+    function getQty() {
+        let qty = 0
+        for (let i = 0; i < cart.length; i += 1){
+            qty += cart[i].qty
+        }
+        return qty
+    }
+
+    function getTotal(){
+        let total = 0
+        for(let i = 0; i < cart.length; i += 1){
+            total += cart[i].price * cart[i].qty
+        }
+        return total
+    }
 }
 
 addItem("Apple",0.99)
 addItem("Orange",2.50)
 addItem("Banana",5.00)
-
+addItem("Apple",0.99)
 
 showItems()
 
